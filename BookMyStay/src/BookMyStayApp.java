a9901b0195c5449772eb3809d45308348d34d649
Parishrut Singh <parishrutsingh314@gmail.com>
2026-04-01 14:14:36 +0530

    Initial commit

---
+++ BookMyStay/src/BookMyStayApp.java
@@ -0,0 +1,12 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        String appName = "BookMyStay";
+        String version = "v1.0";
+
+        System.out.println("Welcome to the " + appName + " Application!");
+        System.out.println("System Version: " + version);
+
+    }
+}
